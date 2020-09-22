6918a7e6a700e71f29cd1cd19c177a97e4659ead
Maksym Maksymenko <6aff21@gmail.com>
2020-09-22 21:49:58 +0300

    Add ReplyContact.

--- src/Messages/Traits/ReplyKeyboardMarkup.php
+++ src/Messages/Traits/ReplyKeyboardMarkup.php
@@ -4,27 +4,45 @@ declare(strict_types=1);
 
 namespace Sokrpro\Notifications\Messages\Traits;
 
+use Closure;
+use Sokrpro\Notifications\Messages\Buttons\TelegramReplyKeyboardRequestContactButton;
+
 trait ReplyKeyboardMarkup
 {
-    public array $rows;
+    public array $keyboard = [];
 
-    public function resize()
+    public bool $resizeKeyboard = false;
+
+    public bool $oneTimeKeyboard = false;
+
+    public bool $selective = false;
+
+    public function resize(): self
     {
+        $this->resizeKeyboard = true;
 
+        return $this;
     }
 
-    public function onetime()
+    public function onetime(): self
     {
+        $this->oneTimeKeyboard = true;
 
+        return $this;
     }
 
     public function selective()
     {
+        $this->selective = true;
 
+        return $this;
     }
 
-    public function button()
+    public function button(Closure $callback, int $row = 0): self
     {
+        $this->keyboard[$row][] = $button = new TelegramReplyKeyboardRequestContactButton();
+        $callback($button);
 
+        return $this;
     }
 }
